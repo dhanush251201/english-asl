106db14f0f1b2e78fa2186c507369613012ce388
dhanush251201 <20z214@psgtech.ac.in>
2024-03-02 22:29:47 +0530

    Completion ULTIMATEv5

--- src/testResults/evaluation.py
+++ src/testResults/evaluation.py
@@ -7,7 +7,13 @@ def getCounts(input:list)->dict:
         "missing":0,
         "searchFound":0,
         "searchMissing":0,
+        "foundPercent":0.0,
+        "missingPercent":0.0,
+        "searchFoundPercent":0.0,
+        "searchMissingPercent":0.0,
+        "after":0,
     }
+
     for i in input:
         RESULTS['tokenCount']=RESULTS["tokenCount"] + i["tokenCount"]
         RESULTS['found'] = RESULTS['found'] + len(i['found'])
@@ -15,6 +21,20 @@ def getCounts(input:list)->dict:
         RESULTS['searchFound'] = RESULTS['searchFound'] + len(i['SimilarityFound'])
         RESULTS['searchMissing'] = RESULTS['searchMissing'] + len(i['SimilarityMissing'])
 
+    RESULTS["foundPercent"] = str((round(RESULTS['found']/RESULTS["tokenCount"] , 5))*100) + "%"
+    RESULTS["missingPercent"] = str((round(RESULTS['missing']/RESULTS["tokenCount"], 5))*100) + "%"
+    try:
+        RESULTS["searchFoundPercent"] = str((round(RESULTS['searchFound']/RESULTS["missing"], 5))*100) + "%"
+    except:
+        pass
+    try:
+        RESULTS["searchMissingPercent"] = str((round(RESULTS['searchMissing']/RESULTS["missing"], 5))*100) + "%"
+    except:
+        pass
+    try:
+        RESULTS["after"] = str((round((RESULTS['found'] + RESULTS['searchFound'])/RESULTS["tokenCount"], 5))*100) + "%"
+    except:
+        pass
     return RESULTS
 
 def main():
@@ -34,7 +54,7 @@ def main():
         "After Enhancements":RESAFTER
     }
     resfile = open("compiled.json",'w')
-    resfile.write(json.dumps(RESULTS, indent=4))
+    resfile.write(json.dumps(RESULTS, indent=2))
 
 if __name__ == '__main__':
     main()
